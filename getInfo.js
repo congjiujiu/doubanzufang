bb46bf890fbc7b49fefc1cc993157d78577b8a77
nighting99 <nighting99@gmail.com>
2016-10-18 16:44:04 +0800

    add detail function

--- getInfo.js
+++ getInfo.js
@@ -6,41 +6,62 @@ const mysql = require('mysql');
 
 let app = new express();
 
-let items = [];
-let ps = [];
-let res = [];
-
 app.get('/', (req,res,next) => {
   res.header('Access-Control-Allow-Origin', '*');
 
-  for (let id = 0; id <= 100; id+=25) {
-    ps.push(new Promise((resolve, reject) => {
-      superagent.get(`https://www.douban.com/group/shanghaizufang/discussion?start=${id}`)
-        .end((err, sres) => {
-          if (err) {
-            return next(err);
-          }
+  let items = [];
+  let ps = [];
+  let type = req.query.type;
 
-          const $ = cheerio.load(sres.text);
+  if (type === 'waterlist') {
+    let start = parseInt(req.query.from, 10);
+    for (let id = start; id <= start+50; id+=25) {
+      ps.push(new Promise((resolve, reject) => {
+        superagent.get(`https://www.douban.com/group/shanghaizufang/discussion?start=${id}`)
+          .end((err, sres) => {
+            if (err) {
+              return next(err);
+            }
 
-          let titles = $('.title > a');
-          for (let i = 0; i < titles.length-1; i++) {
-            items.push({
-              "title": $(titles[i]).attr('title'),
-              "url": $(titles[i]).attr('href')
-            });
-          }
-          resolve(items);
-        });
-      })
-    );
-  };
+            const $ = cheerio.load(sres.text);
 
-  Promise.all(ps).then(items => {
-    let item = items[0];
+            let titles = $('.title > a');
+            for (let i = 0; i < titles.length-1; i++) {
+              items.push({
+                "title": $(titles[i]).attr('title'),
+                "url": $(titles[i]).attr('href')
+              });
+            }
+            resolve(items);
+          });
+        })
+      );
+    };
 
-    res.send(item);
-  });
+    Promise.all(ps).then(items => {
+      let item = items[0];
+
+      res.send(item);
+    });
+  } else if(type === 'detail') {
+    let w_url = req.query.url;
+
+    superagent.get(`https://www.douban.com/group/topic/${w_url}`)
+      .end((err, sres) => {
+        if (err) {
+          return next(err);
+        }
+
+        const $ = cheerio.load(sres.text);
+
+        let content = $('.topic-content').html();
+
+        let response = [];
+        response.push(content);
+
+        res.send(response);
+      });
+  }
 });
 
 app.listen(3000, (req, res) => {
